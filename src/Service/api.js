dc9572839d3ee8d401daf42a2532fcaadfc86799
EdivarAraujo <edivar23araujo@gmail.com>
2022-09-03 11:11:43 -0300

    up

--- src/Service/api.js
+++ src/Service/api.js
@@ -1,7 +1,11 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/'
+  baseURL: `http://${
+    process.env.NODE_ENV != 'production' ? '127.0.0.1' : '192.168.0.11'
+  }:5000`
 })
+
+axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*'
 
 export default api
